823c2d3aee7207869e12a76297c3b557da1387a2
Minh Huy <buiminhhuy13042004@gmail.com>
2024-10-17 01:50:41 +0700

    update
    lấy được link,tên,giá,thuê,số lượng sản phẩm có hàng

--- bookscraper/bookscraper/spiders/product.py
+++ bookscraper/bookscraper/spiders/product.py
@@ -1,23 +1,44 @@
 import scrapy
 
 class ProductSpider(scrapy.Spider):
-    name = "product"  # Đảm bảo tên spider là 'product'
+    name = "product"
     allowed_domains = ["books.toscrape.com"]
     start_urls = ["https://books.toscrape.com/"]
 
+    # Hàm để lấy link từng sản phẩm
     def parse(self, response):
-        # Vòng lặp qua các sản phẩm
-        for product in response.xpath("//ol[@class='row']/li"):
-            link = product.xpath(".//h3/a/@href").get()  # Lấy link sách
+        # Tạo vòng lặp để lập qua các link sản phẩm 
+        for product_link in response.xpath("//ol[@class='row']/li"):
+            link = product_link.xpath(".//h3/a/@href").get()  # Lấy link sách
             full_link = response.urljoin(link)  # Tạo link đầy đủ từ URL tương đối
-            yield {
-                'link': full_link,
-            }
+            
+            # Gửi request đến trang chi tiết sản phẩm và gọi hàm parse_name để cào dữ liệu
+            yield scrapy.Request(url=full_link, callback=self.parse_name)
 
         # Lấy link trang tiếp theo
-        nextpage = response.xpath("//ul[@class='pager']/li[@class='next']/a/@href").get()
-
-        # Nếu có trang tiếp theo, gửi yêu cầu đến đó và tiếp tục thu thập dữ liệu
-        if nextpage:
-            next_page_url = response.urljoin(nextpage)
+        next_page = response.xpath("//ul[@class='pager']/li[@class='next']/a/@href").get()
+        if next_page:
+            next_page_url = response.urljoin(next_page)  # Tạo link đầy đủ từ URL tương đối
             yield scrapy.Request(url=next_page_url, callback=self.parse)
+
+    # Hàm chi tiết sản phẩm 
+    def parse_name(self, response):
+        # Lấy tên sản phẩm từ trang chi tiết sản phẩm
+        product_name = response.xpath("//div[@class='col-sm-6 product_main']/h1/text()").get()
+        product_code = response.xpath("//table[@class='table table-striped']//th[text()='UPC']/following-sibling::td/text()").get()
+        product_type = response.xpath("//table[@class='table table-striped']//th[text()='Product Type']/following-sibling::td/text()").get()
+        product_price =response.xpath("//table[@class='table table-striped']//th[text()='Price (excl. tax)']/following-sibling::td/text()").get()
+        product_price_tax =response.xpath("//table[@class='table table-striped']//th[text()='Price (incl. tax)']/following-sibling::td/text()").get()
+        tax = response.xpath("//table[@class='table table-striped']//th[text()='Tax']/following-sibling::td/text()").get()
+        product_available =response.xpath("//table[@class='table table-striped']//th[text()='Availability']/following-sibling::td/text()").get()
+        # Trả về tên sản phẩm và link tương ứng
+        yield {
+            'url': response.url,
+            'product_name': product_name,
+            'product_code': product_code,
+            'product_type': product_type,
+            'product_price_excl.tax': product_price,
+            'product_price_incl.tax': product_price_tax,
+            'tax': tax,
+            'product_available': product_available
+        }
