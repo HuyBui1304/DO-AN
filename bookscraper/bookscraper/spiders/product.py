26671fb5bf243549e1d9e8f0edbae3255183730b
Minh Huy <buiminhhuy13042004@gmail.com>
2024-10-19 10:58:52 +0700

    update chuyển qua no SQL

--- bookscraper/bookscraper/spiders/product.py
+++ bookscraper/bookscraper/spiders/product.py
@@ -1,44 +1,52 @@
 import scrapy
+import pymongo  # Thư viện để kết nối MongoDB
 
 class ProductSpider(scrapy.Spider):
     name = "product"
     allowed_domains = ["books.toscrape.com"]
     start_urls = ["https://books.toscrape.com/"]
 
-    # Hàm để lấy link từng sản phẩm
+    def __init__(self):
+        # Thiết lập kết nối MongoDB
+        self.client = pymongo.MongoClient("mongodb://localhost:27017/")  # Thay đổi URL nếu MongoDB của bạn chạy ở nơi khác
+        self.db = self.client["books_database"]  # Tên database bạn muốn sử dụng
+        self.collection = self.db["books_collection"]  # Tên collection
+
     def parse(self, response):
-        # Tạo vòng lặp để lập qua các link sản phẩm 
         for product_link in response.xpath("//ol[@class='row']/li"):
-            link = product_link.xpath(".//h3/a/@href").get()  # Lấy link sách
-            full_link = response.urljoin(link)  # Tạo link đầy đủ từ URL tương đối
-            
-            # Gửi yêu cầu đến trang chi tiết sản phẩm và gọi hàm parse_name để cào dữ liệu
+            link = product_link.xpath(".//h3/a/@href").get()
+            full_link = response.urljoin(link)
             yield scrapy.Request(url=full_link, callback=self.parse_name)
 
-        # Lấy link trang tiếp theo
         next_page = response.xpath("//ul[@class='pager']/li[@class='next']/a/@href").get()
         if next_page:
-            next_page_url = response.urljoin(next_page)  # Tạo link đầy đủ từ URL tương đối
+            next_page_url = response.urljoin(next_page)
             yield scrapy.Request(url=next_page_url, callback=self.parse)
 
-    # Hàm chi tiết sản phẩm 
     def parse_name(self, response):
-        # Lấy từng thuộc tính sản phẩm từ trang chi tiết sản phẩm
         product_name = response.xpath("//div[@class='col-sm-6 product_main']/h1/text()").get()
         product_code = response.xpath("//table[@class='table table-striped']//th[text()='UPC']/following-sibling::td/text()").get()
         product_type = response.xpath("//table[@class='table table-striped']//th[text()='Product Type']/following-sibling::td/text()").get()
-        product_price =response.xpath("//table[@class='table table-striped']//th[text()='Price (excl. tax)']/following-sibling::td/text()").get()
-        product_price_tax =response.xpath("//table[@class='table table-striped']//th[text()='Price (incl. tax)']/following-sibling::td/text()").get()
+        product_price = response.xpath("//table[@class='table table-striped']//th[text()='Price (excl. tax)']/following-sibling::td/text()").get()
+        product_price_tax = response.xpath("//table[@class='table table-striped']//th[text()='Price (incl. tax)']/following-sibling::td/text()").get()
         tax = response.xpath("//table[@class='table table-striped']//th[text()='Tax']/following-sibling::td/text()").get()
-        product_available =response.xpath("//table[@class='table table-striped']//th[text()='Availability']/following-sibling::td/text()").get()
-        # Trả về thuộc tính sản phẩm và link tương ứng
-        yield {
+        product_available = response.xpath("//table[@class='table table-striped']//th[text()='Availability']/following-sibling::td/text()").get()
+
+        # Tạo dữ liệu để chèn vào MongoDB
+        product_data = {
             'url': response.url,
             'product_name': product_name,
             'product_code': product_code,
             'product_type': product_type,
-            'product_price_excl.tax': product_price,
-            'product_price_incl.tax': product_price_tax,
+            'product_price_excl_tax': product_price,
+            'product_price_incl_tax': product_price_tax,
             'tax': tax,
             'product_available': product_available
         }
+
+        # Chèn dữ liệu vào MongoDB
+        self.collection.insert_one(product_data)
+
+    def close(self, reason):
+        # Đóng kết nối MongoDB khi spider hoàn tất
+        self.client.close()
