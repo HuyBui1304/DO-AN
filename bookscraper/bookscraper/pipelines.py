c7b6dede419f7831eaa4f8e1d9285d0d7ef8b077
Minh Huy <buiminhhuy13042004@gmail.com>
2024-10-19 12:01:50 +0700

    update

--- bookscraper/bookscraper/pipelines.py
+++ bookscraper/bookscraper/pipelines.py
@@ -1,17 +1,6 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
-# useful for handling different item types with a single interface
-
 import pymongo
-from itemadapter import ItemAdapter
-
 
 class MongoPipeline:
-
     def __init__(self, mongo_uri, mongo_db):
         self.mongo_uri = mongo_uri
         self.mongo_db = mongo_db
@@ -20,23 +9,16 @@ class MongoPipeline:
     def from_crawler(cls, crawler):
         return cls(
             mongo_uri=crawler.settings.get('MONGO_URI'),
-            mongo_db=crawler.settings.get('MONGO_DATABASE', 'scrapy_items')
+            mongo_db=crawler.settings.get('MONGO_DATABASE', 'books_database')
         )
 
     def open_spider(self, spider):
-        # Kết nối tới MongoDB khi spider bắt đầu
         self.client = pymongo.MongoClient(self.mongo_uri)
         self.db = self.client[self.mongo_db]
 
     def close_spider(self, spider):
-        # Đóng kết nối khi spider kết thúc
         self.client.close()
 
     def process_item(self, item, spider):
-        # Chèn item vào collection MongoDB
-        self.db['items'].insert_one(dict(item))
-        return item
-
-class BookscraperPipeline:
-    def process_item(self, item, spider):
+        self.db['books_collection'].insert_one(dict(item))  # 'books_collection' là tên collection
         return item
